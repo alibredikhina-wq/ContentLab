ed269d74c03c3737445eb6682e3ab0e69b46e4ff
Ali Bredikhina <ali.bredikhina@gmail.com>
2026-09-10 18:42:13 +0300

    Fix Yandex Metrika

--- components/YandexMetrika.tsx
+++ components/YandexMetrika.tsx
@@ -10,27 +10,31 @@ export default function YandexMetrika() {
       dangerouslySetInnerHTML={{
         __html: `
           (function(m,e,t,r,i,k,a){
-            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i]=m[i]||function(){
+              (m[i].a=m[i].a||[]).push(arguments)
+            };
             m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {
-              if (document.scripts[j].src === r) { return; }
-            }
+
             k=e.createElement(t);
             a=e.getElementsByTagName(t)[0];
+
             k.async=1;
             k.src=r;
+
             a.parentNode.insertBefore(k,a);
-          })(window, document, "script",
+          })(
+            window,
+            document,
+            "script",
             "https://mc.yandex.ru/metrika/tag.js?id=112462927",
-            "ym");
+            "ym"
+          );
 
           ym(112462927, "init", {
             ssr: true,
-            webvisor: true,
+            webvisor: false,
             clickmap: true,
             ecommerce: "dataLayer",
-            referrer: document.referrer,
-            url: location.href,
             accurateTrackBounce: true,
             trackLinks: true
           });
